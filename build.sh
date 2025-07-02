fbcae92d33940fa300d129049ae7ccd7637b753d
Pravindgholap <pravingholap9110@gmail.com>
2025-07-02 21:29:15 +0530

    Updated only build.sh

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput --clear
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_EMAIL" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ]; then
+    python manage.py createsuperuser --noinput \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL" || true 
+fi
